be85cf44e1dd4549ff9c0cd636f2b8fabcd2966d
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-01-03 12:46:26 -0600

    use bas

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/psyche-interactive",
+
   plugins: [
     react({
       babel: {
